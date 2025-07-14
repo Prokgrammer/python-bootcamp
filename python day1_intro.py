7b19741faa675ddae3e3a9274c20b2c9f9f6ea0f
John Emerson Marquez <emerb5110@gmail.com>
2025-07-13 23:57:13 -0500

    Day 1 – Intro and setup

---
+++ python day1_intro.py
@@ -0,0 +1,6 @@
+name = input("Your Name is?")
+age = input("Your Age is?")
+language = input("Python")
+
+print("Hello " + name + ", you're " + str(age) + " years old.")
+print("Hello " + name + ", youre favorite language is " + str(language))
